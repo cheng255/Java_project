df440ca948a5595ca22271db4a94fc400c8e0f1b
cheng255 <18229094255@163.com>
2020-02-23 12:52:15 +0800

    提交代码

---
+++ java2_15/src/com/atguigu/exer/ComparableCircle.java
@@ -0,0 +1,41 @@
+package com.atguigu.exer;
+
+/*
+ * 继承Circle类并且实现CompareObject接口，在ComparableCircle类中给出接口中方法compareTo的
+ * 实现体，用来比较两个圆的半径大小
+ */
+public class ComparableCircle extends Circle implements CompareObject{
+
+	public ComparableCircle(double radius) {
+		super(radius);
+	}
+	@Override
+	public int compareTo(Object o) {
+	
+		if(this == o) {
+			return 0;
+		}
+		if(o instanceof ComparableCircle) {
+			ComparableCircle c = (ComparableCircle) o;
+//			if(this.getRadius() > c.getRadius()) {
+//				return 1;
+//			}else if(this.getRadius() < c.getRadius()) {
+//				return 0;
+//			}else {
+//				return 0;				
+//			}
+			//当属性radius
+			return this.getRadius().compareTo(c.getRadius());
+		}else {
+//			return 0;
+			throw new RuntimeException("传入的数据类型不匹配");
+		}
+
+		
+		
+		
+		
+	}
+
+	
+}
