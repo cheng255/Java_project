9a769f0dcbbf9c1b53e76c68bb1fd572dd97c9a1
cheng255 <18229094255@163.com>
2021-03-19 15:30:49 +0800

    测试

---
+++ javaSenior/day02/exer2/synchronizedTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.exer2;
+
+/**
+ * @author nuonuo
+ * @create 2021-03-19 14:58
+ */
+public class synchronizedTest {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("abc");
+        sb.append("d").append("e").append("f");
+    }
+}
